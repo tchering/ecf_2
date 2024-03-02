196b317d086e075806289a0a206e6376c3aae7c7
tchering <sonam.sherpa.lama@gmail.com>
2024-03-02 12:00:12 +0100

    individu crud

---
+++ public/js/myScript.js
@@ -0,0 +1,60 @@
+function previewImage(event, id_image) {
+  const image = event.target.files[0];
+  if (image) {
+    const elt_image = document.getElementById(id_image);
+    elt_image.src = URL.createObjectURL(image);
+  }
+}
+function popupCenter(url, title, w, h) {
+  var left = screen.width / 2 - w / 2;
+  var top = screen.height / 2 - h / 2;
+  return window.open(
+    url,
+    title,
+    "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=" +
+      w +
+      ", height=" +
+      h +
+      ", top=" +
+      top +
+      ", left=" +
+      left
+  );
+}
+
+function getIdChecked(name_element) {
+  let checkboxes = document.getElementsByName(name_element);
+  let id = 0;
+  checkboxes.forEach((item) => {
+    if (item.checked == true) {
+      id = item.value;
+      stop;
+    }
+  });
+  return id;
+}
+function onlyOne(checkbox) {
+  //  checkbox est l'element où on a cliqué
+  let checkboxes = document.getElementsByName(checkbox.name);
+  checkboxes.forEach(function (item) {
+    if (item != checkbox) {
+      // tester si item l'un des elements de checkboxes est different de checkbox selectionnée
+      item.checked = false;
+    }
+  });
+  checkbox.checked = true;
+}
+const touche = (event) => {
+  if (event.key == "Enter") {
+    chercher();
+  }
+};
+
+const lancerLoader = () => {
+  div_loader.classList.toggle("d-none");
+  // div_loader.classList.remove('d-none');
+};
+const terminerLoader = () => {
+  div_loader.classList.toggle("d-none");
+  // div_loader.classList.add('d-none');
+};
--- src/Controller/IndividuController.php
+++ src/Controller/IndividuController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Individu;
+use App\Form\IndividuType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,24 +13,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class IndividuController extends AbstractController
 {
     #[Route('/individu', name: 'app_individu')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $individus = $em->getRepository(Individu::class)->findAll();
         return $this->render('individu/index.html.twig', [
             'controller_name' => 'IndividuController',
+            'individus' => $individus
         ]);
     }
 
-    #[Route('/individu/add/{id}',name:'app_individu_add')]
-    public function add(EntityManagerInterface $em, Request $request,$id){
-        $individu = $em->getRepository(Individu::class)->find($id);
+    #[Route('/individu/add/{id}', name: 'app_individu_add')]
+    public function add(EntityManagerInterface $em, Request $request, $id)
+    {
         $id =  (int) $id;
-        if($id==0){
-            $individu = new Individu();
+        if ($id) {
+            $individu = $em->getRepository(Individu::class)->find($id);
         } else {
-
+            $individu = new Individu();
         }
-        return $this->render('individu/form.html.twig',[
+        $form = $this->createForm(IndividuType::class, $individu);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($individu);
+            $em->flush();
+            return $this->redirectToRoute('app_individu');
+        }
+        return $this->render('individu/form.html.twig', [
+            'form' => $form
         ]);
+    }
+
+    #[Route('/individu/delete/{id}', name: 'app_individu_delete')]
+    public function delete(EntityManagerInterface $em, $id)
+    {
+        $individu = $em->getRepository(Individu::class)->find($id);
+        $em->remove($individu);
+        $em->flush();
+        return $this->redirectToRoute('app_individu');
     }
 }
